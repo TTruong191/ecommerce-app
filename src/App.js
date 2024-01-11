88d074e777fb4f5bc66aba51605c8ea62d00e0e5
totruong <totruong591@gmail.com>
2024-01-11 21:09:39 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import Product from "./Product";
+import Products from "./Products";
 import Cart from "./Cart";
 import SingleProduct from "./SingleProduct";
 import ErrorPage from "./ErrorPage";
@@ -43,7 +43,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Product />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/singleproduct/:id" element={<SingleProduct />} />
           <Route path="/*" element={<ErrorPage />} />
